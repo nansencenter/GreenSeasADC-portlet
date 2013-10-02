8075b953cd68575585a302a99704d49892401330
Aleksander Vines <aleksandervines1@hotmail.com>
2013-10-02 11:21:03 +0200

    Fixed Issue #1:describeFeatureType XML parsing in I.E The XML parsing should now work in I.E 10, Firefox 24 and Chrome 29.

--- docroot/js/src/XMLParser.js
+++ docroot/js/src/XMLParser.js
@@ -4,21 +4,35 @@ var debugXML = true;// debug flag
 
 myNamespace.XMLParser = (function($) {
 
+	function findBrowser() {
+		var ua = navigator.userAgent, N = navigator.appName, tem, M = ua
+				.match(/(opera|chrome|safari|firefox|msie|trident)\/?\s*([\d\.]+)/i)
+				|| [];
+		M = M[2] ? [ M[1], M[2] ] : [ N, navigator.appVersion, '-?' ];
+		if (M && (tem = ua.match(/version\/([\.\d]+)/i)) != null)
+			M[2] = tem[1];
+		return M[0];// M.join(' ');
+	}
+
 	function extractParameters(xmlSchema) {
 		console.log("extractParameters");
+		var browser = findBrowser();
+		console.log("browser: " + browser);
 		console.log(xmlSchema);
-		var oParser = new DOMParser();
-		var xmlDoc = xmlSchema.responseXML;
+		var parameters = [];
+		var xmlDoc
+		if (browser == "Chrome" || browser == "Firefox") {
+			xmlDoc = xmlSchema.responseXML;
+		} else {
+			xmlDoc = xmlSchema._object.responseXML;
+		}
 		console.log(xmlDoc);
-		// var xmlDoc = oParser.parseFromString(xmlSchema.responseText,
-		// "text/xml");
 
 		if (debugXML) {
 			console.log(xmlDoc.documentElement.childNodes[3].childNodes[1].childNodes[1].childNodes[1].childNodes[1]
 					.getAttribute("name"));
 		}
 
-		var parameters = [];
 		var sequence = xmlDoc.documentElement.childNodes[3].childNodes[1].childNodes[1].childNodes[1].childNodes;
 		var numberOfParameters = sequence.length;
 		for ( var i = 1; i < numberOfParameters; i += 2) {
@@ -29,6 +43,7 @@ myNamespace.XMLParser = (function($) {
 			console.log("parameters: " + parameters);
 		// getting the table name:
 		parameters.push(xmlDoc.documentElement.childNodes[5].getAttribute("name"));
+
 		return parameters;
 	}
 	// public interface
