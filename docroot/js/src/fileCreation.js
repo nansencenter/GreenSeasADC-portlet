cfc54e1bbedc6921d467cdcd7ae3f68471438c3a
Aleksander Vines <aleksandervines1@hotmail.com>
2013-10-09 14:32:17 +0200

    removed spaces after ;

--- docroot/js/src/fileCreation.js
+++ docroot/js/src/fileCreation.js
@@ -5,12 +5,12 @@ var debugfC = false;// debug flag
 myNamespace.fileCreation = (function($) {
 
 	function createCSV(dataIn){
-		var csvContent = "ID; Lat(dec.deg); Long(dec.deg); Area; Depth of Sea (m); Depth of Sample (m); Date; Time";
+		var csvContent = "ID;Lat(dec.deg);Long(dec.deg);Area;Depth of Sea (m);Depth of Sample (m);Date;Time";
 		$.each(myNamespace.handleParameters.chosenParameters.tablesSelected, function(k, table) {
 			$.each(myNamespace.handleParameters.chosenParameters.parametersByTable[table], function(j, parameter) {
 				if (debugfC)
 					console.log(parameter);
-				csvContent += "; "+ myNamespace.handleParameters.getHeader(parameter, table);
+				csvContent += ";"+ myNamespace.handleParameters.getHeader(parameter, table);
 			});
 		});
 		csvContent += "\n";
@@ -18,9 +18,9 @@ myNamespace.fileCreation = (function($) {
 			console.log("Added headers: " + csvContent);
 		$.each(dataIn, function(i, val) {
 			var properties = val.properties;
-			csvContent += val.id + "; ";
+			csvContent += val.id + ";";
 			var pos = val.geometry.coordinates;
-			csvContent += pos[0] + "; ";
+			csvContent += pos[0] + ";";
 			csvContent += pos[1] + "";
 			
 			for (prop in properties) {
@@ -28,7 +28,7 @@ myNamespace.fileCreation = (function($) {
 				var value = properties[prop];
 				if (value == null)
 					value = "";
-				csvContent += "; " + value;
+				csvContent += ";" + value;
 			}
 			
 			
