bcea3f635ad4f68eb2b4b3bde9789a477a32b638
Aleksander Vines <aleksandervines1@hotmail.com>
2013-10-11 14:43:47 +0200

    Fixed another bug with the exported headers not beeing sorted correctly.

--- docroot/js/src/fileCreation.js
+++ docroot/js/src/fileCreation.js
@@ -1,25 +1,25 @@
 var myNamespace = myNamespace || {};
 
-var debugfC = true;// debug flag
+var debugfC = false;// debug flag
 
 myNamespace.fileCreation = (function($) {
 
-	function createCSV(dataIn) {
-		var csvContent = "ID;Lat(dec.deg);Long(dec.deg);Area;Depth of Sea (m);Depth of Sample (m);Date;Time";
-		$.each(myNamespace.handleParameters.chosenParameters.tablesSelected, function(k, table) {
-			$.each(myNamespace.handleParameters.chosenParameters.parametersByTable[table], function(j, parameter) {
-				if (debugfC)
-					console.log(parameter);
-				csvContent += ";" + myNamespace.handleParameters.getHeader(parameter, table);
-				if (myNamespace.handleParameters.qf)
-					csvContent += ";" + qfHeader;
-				else {
-					if (debugfC)
-						console.log("Not QF! " + myNamespace.handleParameters.qf);
-				} 
-					
-			});
+	function createCSVHeader(headers) {
+		var headerString = "";
+		var delimiter = "";
+		$.each(headers, function(i, val) {
+			headerString += delimiter;
+			delimiter = ";";
+			headerString += val;
 		});
+
+		return headerString;
+	}
+	function createCSV(dataIn) {
+		var csvContent = createCSVHeader(myNamespace.handleParameters.mainParameters.basicHeader.concat(
+				myNamespace.handleParameters.mainParameters.customHeader).concat(
+				myNamespace.handleParameters.getChosenHeader()));
+
 		csvContent += "\n";
 		if (debugfC)
 			console.log("Added headers: " + csvContent);
