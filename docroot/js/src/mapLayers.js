0859e663a5640b06538ab55c3f2502dc6ca5c9e6
unknown <alevin@.ad.nersc.no>
2014-02-12 11:51:03 +0100

    comment

--- docroot/js/src/mapLayers.js
+++ docroot/js/src/mapLayers.js
@@ -17,7 +17,11 @@ myNamespace.mapLayers = (function(jQ, bH) {
 		activeLayers++;
 	}
 
-	/* Small function to create a short random string to identify a request */
+	/*
+	 * Small function to create a short random string to identify a request -
+	 * from
+	 * http://stackoverflow.com/questions/8532406/create-a-random-token-in-javascript-based-on-user-details
+	 */
 	function rand() {
 		return Math.random().toString(36).substr(2); // remove `0.`
 	}
