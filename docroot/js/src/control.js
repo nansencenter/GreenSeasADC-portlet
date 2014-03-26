da31177f29f39cb0bef88a160b7fc8175c1c28c1
unknown <alevin@.ad.nersc.no>
2014-03-26 14:08:40 +0100

    moved functioncall to mapViewer init

--- docroot/js/src/control.js
+++ docroot/js/src/control.js
@@ -53,8 +53,6 @@ myNamespace.control = (function($, OL, ns) {
 		regenerateSavedQuery()
 
 		$('#gsadbcPortlet [title]').qtip();
-		//TODO: make it possible to disable this
-		setInterval(function(){myNamespace.mapViewer.checkLoadingOfLayers()},1000);
 	}
 
 	function regenerateSavedQuery() {
