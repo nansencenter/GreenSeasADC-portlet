fe46925ab3f94afc1d2eb91f46df0452abb8a1b4
unknown <alevin@.ad.nersc.no>
2014-06-27 12:04:07 +0200

    cleaned up

--- docroot/js/src/fileCreation.js
+++ docroot/js/src/fileCreation.js
@@ -256,32 +256,6 @@ myNamespace.fileCreation = (function($, ns) {
 		ns.ajax.aui(data, success, failure);
 	}
 	
-	function downloadFile(sUrl) {
-		isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-		isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
-	    //If in Chrome or Safari - download via virtual link click
-	    if (isChrome || isSafari) {
-	        //Creating new link node.
-	        var link = document.createElement('a');
-	        link.href = sUrl;
-	 
-	        if (link.download !== undefined){
-	            //Set HTML5 download attribute. This will prevent file from opening if supported.
-	            var fileName = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
-	            link.download = fileName;
-	        }
-	 
-	        //Dispatching click event.
-	        if (document.createEvent) {
-	            var e = document.createEvent('MouseEvents');
-	            e.initEvent('click' ,true ,true);
-	            link.dispatchEvent(e);
-	            return true;
-	        }
-	    }
-	    window.open(sUrl);
-	}
-
 	function createNetCDFGrid(features, parameter, timeResolution, latLonResolution) {
 		// TODO: QF
 		var latArray = [];
